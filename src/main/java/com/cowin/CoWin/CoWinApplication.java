5b83d9644c08bf5b0eabfa8315c260a972f96e4f
Rajeev Loghade <rajeevloghade010@gmail.com>
2021-05-14 12:28:39 +0530

    Initial Commit For Project Deployement

---
+++ src/main/java/com/cowin/CoWin/CoWinApplication.java
@@ -0,0 +1,13 @@
+package com.cowin.CoWin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoWinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoWinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cowin/CoWin/CoWinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cowin.CoWin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoWinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
